dd244c4a69915df74d0a921af3087d698afcfb50
kriviears <kriviears@gmail.com>
2018-12-17 16:19:43 -0500

    finished commit

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const data = require('./db/notes');
 const app = express();
 
 // ADD STATIC SERVER HERE
+app.use(express.static('public'));
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
