88a97a54e3218904577c7721cc42f2728ac35f47
kriviears <kriviears@gmail.com>
2018-12-17 16:15:17 -0500

    public files now being served up by express static

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const data = require('./db/notes');
+
+const app = express();
+
+// ADD STATIC SERVER HERE
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
